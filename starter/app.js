e9318d7020998758d2c60fc2f0368aac0fd07be8
nattanjunior <fnatanieljunior@gmail.com>
2024-11-20 15:11:43 -0300

    intial commit

---
+++ starter/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import tasks from './routes/task.js'
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.json())
+
+//routes 
+app.use('/api/v1/tasks', tasks)
+
+app.listen(port, () => console.log(`server running ${port}`))
