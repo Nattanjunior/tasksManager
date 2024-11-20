35800eb5a1d4d855093449d0ac159d394dc5890f
nattanjunior <fnatanieljunior@gmail.com>
2024-11-20 17:25:43 -0300

    config de routes feitas, get, post patch e delete

--- starter/routes/task.js
+++ starter/routes/task.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import { getAllTasks, createTask, getTask, updateTask, deleteTask } from '../controllers/tasks.js';
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  res.json({message: 'all items'})
-})
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 export default router
 
