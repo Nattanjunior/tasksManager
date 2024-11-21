518b23cfce07d42de7ec28d32715b27396b0fa5d
nattanjunior <fnatanieljunior@gmail.com>
2024-11-21 15:53:26 -0300

    adicionado middleware express.static()

--- starter/app.js
+++ starter/app.js
@@ -1,13 +1,25 @@
 import express from "express";
 import tasks from './routes/task.js'
-import './db/connect.js'
+import connectDB from "./db/connect.js";
+import 'dotenv/config'
 const app = express();
 const port = 3000;
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 //routes 
 app.use('/api/v1/tasks', tasks)
 
-app.listen(port, () => console.log(`server running ${port}`))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`server running ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
+
