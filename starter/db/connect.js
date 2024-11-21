84b43ea9f6140d166fb04c24f551fa06c5d128bf
nattanjunior <fnatanieljunior@gmail.com>
2024-11-21 15:57:24 -0300

    propriedades passadas para o mongoose

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+}
 
-const connectionString = 'mongodb+srv://fnatanieljunior:Futuromilionario@api-task-manager.zsdfj.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&appName=Api-task-manager';
-
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-})
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((error) => console.log(error))
+export default connectDB
