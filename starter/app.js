b4e2435854a12fbfa45a578b1ae6c717ebf7d172
nattanjunior <fnatanieljunior@gmail.com>
2024-11-24 14:32:44 -0300

    const port alterada

--- starter/app.js
+++ starter/app.js
@@ -1,16 +1,23 @@
 import express from "express";
+
 import tasks from './routes/task.js'
 import connectDB from "./db/connect.js";
+import { notFound } from "./middleware/not-found.js";
+import { errorHandlerMiddleware } from "./middleware/error-handler.js";
+
 import 'dotenv/config'
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(express.static('./public'))
-app.use(express.json())
+app.use(express.static('./public'));
+app.use(express.json());
 
 //routes 
-app.use('/api/v1/tasks', tasks)
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
   try {
